a87b5537af6a576b5519f4739e5821df9b6a99ad
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-12-03 00:10:57 +0600

    Update f_sub.py

--- body/f_sub.py
+++ body/f_sub.py
@@ -22,7 +22,7 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 ✇ Join Our Updates Channel ✇ 📢", url=f"https://t.me/{FORCE_SUB}") ]]
+    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{FORCE_SUB}") ]]
     text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
     photo_url = "https://envs.sh/CEG.jpg"  # এখানে আপনার ইমেজ লিংক ব্যবহার করুন
 
